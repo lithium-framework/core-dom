2df4021613699b666cafc11526a2ab4d64eefff4
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-05-29 21:21:53 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+export {
+  Directive,
+  asyncAppend , AsyncAppendDirective,
+  asyncReplace , AsyncReplaceDirective,
+  cache , CacheDirective,
+  choose,
+  classMap , ClassInfo , ClassMapDirective,
+  guard , GuardDirective,
+  ifDefined,
+  join,
+  keyed , Keyed,
+  live , LiveDirective,
+  map,
+  range,
+  ref , Ref , RefDirective , createRef , RefOrCallback,
+  repeat , RepeatDirective , RepeatDirectiveFn,
+  styleMap , StyleMapDirective,
+  templateContent , TemplateContentDirective,
+  unsafeHTML , UnsafeHTMLDirective, 
+  unsafeSVG , UnsafeSVGDirective,
+  until , UntilDirective,
+  when,
+  useState , UseStateDirective,
+  useStyle , UseStyleDirective,
+  useEffect , UseEffectDirective
+} from './directives/index.js';
+
+export {
+
+} from './decorators/index.js';
+
+export { 
+  html, 
+  css,
+  TemplateResult, 
+  TemplateInstance  
+} from 'lit';
+
+import { render as lit_render , TemplateResult } from 'lit';
+
+export function render( element:TemplateResult , parent:HTMLElement | DocumentFragment = document.createDocumentFragment() ){
+  return lit_render( element , parent );
+}
